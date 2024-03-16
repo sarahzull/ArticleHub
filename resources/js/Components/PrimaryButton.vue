fa6e4c7021794e7e803e6f590d2050b0a0200992
sarahzull <s4rhzl@gmail.com>
2024-03-16 13:27:52 +0800

    feat: update primary button add disabled props

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,7 +1,25 @@
 <template>
-    <button
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-    >
-        <slot />
-    </button>
+  <button
+    :class="[
+      'inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150',
+      {
+        'bg-gray-800 text-white': !disabled,
+        'bg-gray-300 text-gray-500 cursor-not-allowed': disabled,
+      },
+    ]"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
 </template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
