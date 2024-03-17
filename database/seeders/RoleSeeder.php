783a448612bd0585e4aa93aff2a6eb6dd1d28390
sarahzull <s4rhzl@gmail.com>
2024-03-17 22:28:51 +0800

    feat: add role seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'Admin'],
+            ['name' => 'Editor'],
+            ['name' => 'User'],
+        ];
+
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+    }
+}
