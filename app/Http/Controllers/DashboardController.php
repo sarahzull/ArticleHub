4f76f137fe763b31dc223f1e9ed1bb83c3e4b103
sarahzull <s4rhzl@gmail.com>
2024-03-21 18:08:24 +0800

    feat: add articles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,27 @@ class DashboardController extends Controller
 {
     public function index () 
     {
-        return Inertia::render('Dashboard');
-    }
+        $articles = Article::with('author', 'category')
+            ->latest()
+            ->limit(5)
+            ->get();
+
+        $user = auth()->user();
+        if ($user) {
+            if ($user->subscription) {
+                $userWithSubscription = $user->load('subscription.plan');
+                $currentPlan = optional($userWithSubscription->subscription->plan)->name ?? 'Free';
+            } else {
+                $currentPlan = 'Free';
+            }
+        } else {
+            $currentPlan = 'Free';
+        }
+
+        return Inertia::render('Dashboard', [
+            'articles' => $articles,
+            'currentPlan' => $currentPlan
+        ]);}
 
     public function redirect () 
     {
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,11 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({
+    articles: Object,
+    currentPlan: Object,
+});
 </script>
 
 <template>
@@ -8,13 +13,29 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">Dashboard</h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div v-if="currentPlan == 'Free'" class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+        
+        <div v-else class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div v-for="article in articles" :key="article.id" class="mb-8">
+                            <div class="mb-2">
+                                <h3 class="text-2xl font-bold">{{ article.title }}</h3> by 
+                                <span class="text-gray-500">{{ article.author.name }}</span>
+                            </div>
+                            <div v-html="article.content" class="text-gray-700"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
