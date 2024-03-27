9d2f46200917bf96aa28921bbb7da60aeec8611f
sarahzull <s4rhzl@gmail.com>
2024-03-27 13:32:33 +0800

    feat: update database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ArticleSeeder::class);
     }
 }
