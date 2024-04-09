0df3633538497f2819b3f5823b9eb7c53decfc01
sarahzull <s4rhzl@gmail.com>
2024-04-09 15:57:13 +0800

    feat: some update

--- app/Handler/DefaultRespondsTo.php
+++ app/Handler/DefaultRespondsTo.php
@@ -13,7 +13,6 @@ class DefaultRespondsTo implements RespondsToWebhook
     public function respondToValidWebhook(Request $request, WebhookConfig $config): Response
     {
         Log::info("request", $request->all());
-        Log::info("config", $config);
         return response()->json(['message' => 'ok']);
     }
 }
