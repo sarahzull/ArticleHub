7b05da481adf135e72fca0ae05e36439022e3e19
sarahzull <s4rhzl@gmail.com>
2024-04-09 15:34:58 +0800

    feat: update signature validation

--- app/Handler/XsollaSignature.php
+++ app/Handler/XsollaSignature.php
@@ -10,47 +10,28 @@ use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
 
 class XsollaSignature implements SignatureValidator
 {
-    // public function isValid(Request $request, WebhookConfig $config): bool
-    // {
-    //     $payload = $request->getContent(); 
-    //     $secret = $config->signingSecret;
-        
-    //     $authorizationHeader = $request->header($config->signatureHeaderName);
-    //     Log::debug('Authorization header', ['header' => $authorizationHeader]);
-
-    //     if (strpos($authorizationHeader, 'Signature ') === 0) {
-    //         $signatureSent = substr($authorizationHeader, strlen('Signature '));
-    //         Log::debug('Signature sent', ['signature' => $signatureSent]);
-    //     } else {
-    //         return false;
-    //     }
-
-    //     $concatenated = $payload . $secret;
-    //     $generatedSignature = sha1($concatenated);
-    //     Log::debug("generatedSignature", ['signature' => $generatedSignature]);
-
-    //     Log::debug('Hash equals', ['result' => hash_equals($generatedSignature, $signatureSent)]);
-
-    //     if (hash_equals($generatedSignature, $signatureSent)) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        $signature = $request->header($config->signatureHeaderName);
-        if (! $signature) {
+        $payload = $request->getContent(); 
+        $secret = $config->signingSecret;
+        
+        $authorizationHeader = $request->header($config->signatureHeaderName);
+
+        if (strpos($authorizationHeader, 'Signature ') === 0) {
+            $signatureSent = substr($authorizationHeader, strlen('Signature '));
+        } else {
             return false;
         }
-        $signature = trim(str_replace("sha1=", "", $signature));
-        $signingSecret = $config->signingSecret;
 
-        if (empty($signingSecret)) {
-            throw WebhookFailed::signingSecretNotSet();
+        $concatenated = $payload . $secret;
+        $generatedSignature = sha1($concatenated);
+
+        Log::debug('Hash equals', ['result' => hash_equals($generatedSignature, $signatureSent)]);
+
+        if (hash_equals($generatedSignature, $signatureSent)) {
+            return true;
+        } else {
+            throw new WebhookFailed('Invalid signature.');
         }
-        $computedSignature = hash_hmac('sha1', $request->getContent(), $signingSecret);
-        return hash_equals($signature, $computedSignature);
     }
 }
