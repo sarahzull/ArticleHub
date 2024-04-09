b53cd613a18807a7d69f62d328447d46a301a9bc
sarahzull <s4rhzl@gmail.com>
2024-04-09 15:49:27 +0800

    feat: update log config

--- app/Handler/DefaultRespondsTo.php
+++ app/Handler/DefaultRespondsTo.php
@@ -13,16 +13,7 @@ class DefaultRespondsTo implements RespondsToWebhook
     public function respondToValidWebhook(Request $request, WebhookConfig $config): Response
     {
         Log::info("request", $request->all());
-        Log::info("config", json_encode($config));
+        Log::info("config", $config);
         return response()->json(['message' => 'ok']);
-    }
-
-    public function respondToInvalidWebhook(Request $request, WebhookConfig $config, $reason = 'Invalid signature'): Response
-    {
-        Log::warning("Invalid webhook request received", [
-            'reason' => $reason,
-            'data' => $request->all()
-        ]);
-        return response()->json(['message' => 'not ok', 'error' => $reason], 400);
     }
 }
