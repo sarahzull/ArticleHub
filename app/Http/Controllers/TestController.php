cb26d5540aa6f013702efc53911e1962e7057486
sarahzull <s4rhzl@gmail.com>
2024-04-19 12:28:36 +0800

    feat: add view all subs

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,5 +11,26 @@ use Illuminate\Support\Facades\Config;
 
 class TestController extends Controller
 {
-    //
+    public function getSubscriptions ()
+    {
+        $merchantId = Config::get('services.xsolla.merchant_id');
+        $merchantApiKey = Config::get('services.xsolla.merchant_api_key');
+        $url = Config::get('services.xsolla.api_url') . "merchants/" . $merchantId . "/subscriptions";
+
+        $params = [
+            'limit' => 10,
+        ];
+
+        $response = Http::withBasicAuth($merchantId, $merchantApiKey)
+        ->withHeaders(['Content-Type' => 'application/json'])
+        ->get($url, $params);
+
+        if ($response->successful()) {
+            $data = $response->json();
+            return response()->json($data);
+        } else {
+            return response()->json(['error' => 'Request failed.'], $response->status());
+        }
+
+    }
 }
